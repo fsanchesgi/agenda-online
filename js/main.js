a16a92e7ade6a954447df9e51c2cc0decafa16b4
fsanchesgi <133274266+fsanchesgi@users.noreply.github.com>
2025-12-16 06:22:59 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+<!-- ============================= -->
+<!-- ⚙️ js/main.js -->
+<!-- ============================= -->
 
+
+function registerUser(event) {
+event.preventDefault();
+alert('Conta criada! (simulação)');
+window.location.href = 'dashboard.html';
+}
+
+
+function createAppointment(event) {
+event.preventDefault();
+alert('Agendamento confirmado! (simulação)');
+}
