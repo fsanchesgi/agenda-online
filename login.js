c076a2f0e9e8b11a477e0ff8164ba83a5fd82379
fsanchesgi <133274266+fsanchesgi@users.noreply.github.com>
2025-12-17 10:23:53 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,89 +1,18 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-  <meta charset="UTF-8" />
-  <title>Login | TimelyPro</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+const SUPABASE_URL = "https://uqwbduinwugaqexsvkxc.supabase.co";
+const SUPABASE_ANON_KEY =
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxd2JkdWlud3VnYXFleHN2a3hjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU3MTk5MjMsImV4cCI6MjA4MTI5NTkyM30._GzXlkNAvqbevYjmi-crhvSKGQQfX3yjzTWT5PTvIxE";
 
-  <link rel="icon" href="images/timelypro-favicon.png" />
-  <link rel="stylesheet" href="style.css" />
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
 
-  <!-- Supabase v2 -->
-  <script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
-</head>
+async function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
-<body class="auth-page">
+  const { error } = await supabaseClient.auth.signInWithPassword({ email, password });
 
-  <header class="auth-header">
-    <img src="images/timelypro-logo.png" class="logo logo-auth" />
-  </header>
-
-  <main class="auth-container">
-    <h1>Acessar conta</h1>
-
-    <form id="loginForm" class="auth-form">
-      <input
-        type="email"
-        id="email"
-        placeholder="Digite seu e-mail"
-        required
-      />
-
-      <input
-        type="password"
-        id="password"
-        placeholder="Digite sua senha"
-        required
-      />
-
-      <button type="submit" class="btn-primary">
-        Entrar
-      </button>
-    </form>
-
-    <a href="index.html" class="link-back">
-      ← Voltar para página inicial
-    </a>
-  </main>
-
-  <script>
-    // =========================
-    // SUPABASE CLIENT
-    // =========================
-    const SUPABASE_URL = "https://uqwbduinwugaqexsvkxc.supabase.co";
-    const SUPABASE_ANON_KEY =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxd2JkdWlud3VnYXFleHN2a3hjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU3MTk5MjMsImV4cCI6MjA4MTI5NTkyM30._GzXlkNAvqbevYjmi-crhvSKGQQfX3yjzTWT5PTvIxE";
-
-    const supabase = window.supabase.createClient(
-      SUPABASE_URL,
-      SUPABASE_ANON_KEY
-    );
-
-    // =========================
-    // LOGIN
-    // =========================
-    document
-      .getElementById("loginForm")
-      .addEventListener("submit", async (e) => {
-        e.preventDefault();
-
-        const email = document.getElementById("email").value;
-        const password = document.getElementById("password").value;
-
-        const { data, error } = await supabase.auth.signInWithPassword({
-          email,
-          password,
-        });
-
-        if (error) {
-          alert("Erro ao fazer login: " + error.message);
-          return;
-        }
-
-        // 🔥 LOGIN OK → REDIRECIONA
-        window.location.href = "dashboard.html";
-      });
-  </script>
-
-</body>
-</html>
+  if (error) alert(error.message);
+  else window.location.href = "/dashboard.html";
+}
