94e98b5ede310cacabfd4710ad5c00c58d9244ff
fsanchesgi <133274266+fsanchesgi@users.noreply.github.com>
2025-12-17 10:16:13 -0300

    Delete lib directory

--- lib/app/login/app/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function DashboardPage() {
-  return (
-    <div>
-      <h1>Dashboard TimelyPro</h1>
-    </div>
-  )
-}
--- lib/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createBrowserClient } from '@supabase/ssr'
-
-export const supabase = createBrowserClient(
-  'https://uqwbduinwugaqexsvkxc.supabase.co',
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
